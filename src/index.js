32f1b4220539996dbf361ebc3fec515bb3c33dc5
Maxim Mikhailov <makmikhailov@gmail.com>
2020-10-15 19:35:18 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+  if (n < 0) return str.slice(1).split('').reverse().join('');
+  return str.split('').reverse().join('');
 }
